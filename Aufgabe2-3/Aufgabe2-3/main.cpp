86f889580f429077a535a8c0a726b47780a20d1a
herrcao <herrcao@gmail.com>
2017-05-10 14:51:59 +0200

    Foto

---
+++ Aufgabe2-3/Aufgabe2-3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Aufgabe2-3
+//
+//  Created by herrcao on 2017/5/10.
+//  Copyright © 2017年 herrcao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
